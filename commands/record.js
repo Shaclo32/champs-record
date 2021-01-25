f031071ae835b23581a1f7c23d245e2aba774c88
Shaclo32 <69423125+Shaclo32@users.noreply.github.com>
2021-01-25 23:07:17 +0700

    Update record.js

--- commands/record.js
+++ commands/record.js
@@ -3,15 +3,14 @@ const fs = require("fs");
     module.exports.run = async (client, message, args) => {
 
         const voicechannel = message.member.voice.channel;
-        if (!voicechannel) return message.channel.send("Please join a voice channel first!");
+        if (!voicechannel) return message.channel.send("Сначала подключитесь к голосовому каналу");
 
         const connection = await message.member.voice.channel.join();
         const audio = connection.receiver.createStream(message.member, { mode: 'opus', end: 'silence'});
 
         audio.pipe(fs.createWriteStream(`./recorded-${message.author.id}.pcm`));
         audio.on("end", () => {
-
-            message.channel.send("Finished writing audio");
+            message.channel.send("Запись окончена");
         });
 
     }
