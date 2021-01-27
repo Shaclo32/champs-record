1d8ca43843794a353a15a1bacdf1d9e11a9f72ca
Shaclo32 <69423125+Shaclo32@users.noreply.github.com>
2021-01-27 22:24:32 +0700

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 module.exports.run = async (client, message, args) => {
 
     const voicechannel = message.member.voice.channel;
-    if (!voicechannel) return message.channel.send("Please join a voice channel!");
+    if (!voicechannel) return message.channel.send("Подключись сначала к голосовому каналу");
 
-    if (!fs.existsSync(`./recorded-${message.author.id}.pcm`)) return message.channel.send("Аудио не записано");
+    if (!fs.existsSync(`./recorded-${message.author.id}.pcm`)) return message.channel.send("Ваше аудио не записано");
 
     const connection = await message.member.voice.channel.join();
     const stream = fs.createReadStream(`./recorded-${message.author.id}.pcm`);
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) => {
 
     dispatcher.on("finish", () => {
         message.member.voice.channel.leave();
-        return message.channel.send("Окончание проигрования записи");
+        return message.channel.send("Окончание записи аудио");
     })
 }
 
--- commands/record.js
+++ commands/record.js
@@ -1,20 +1,24 @@
-const fs = require("fs");
+const fs = require('file-system');
+const {MessageAttachment} = require('discord.js')
 
-    module.exports.run = async (client, message, args) => {
 
-        const voicechannel = message.member.voice.channel;
-        if (!voicechannel) return message.channel.send("Сначала подключитесь к голосовому каналу");
+module.exports.run = async (client,message,args) => {
+    const voicechannel = message.member.voice.channel;
+    if (!voicechannel) return message.channel.send("Подключитесь сначала к голосовому каналу");
 
-        const connection = await message.member.voice.channel.join();
-        const audio = connection.receiver.createStream(message.member, { mode: 'opus', end: 'silence'});
+    const connection = await message.member.voice.channel.join();
+    const receiver = connection.receiver.createStream(message.member,{
+        mode:'pcm',
+    });
 
-        audio.pipe(fs.createWriteStream(`./recorded-${message.author.id}.pcm`));
-        audio.on("end", () => {
-            message.channel.send("Запись окончена");
-        });
+    const writer = receiver.pipe(fs.createWriteStream(`./recorded-${message.author.id}.pcm`));
+    writer.on("finish", () => {
+        message.member.voice.channel.leave();
+        message.channel.send("Конец записи");
+    });
 
-    }
+}
 
-    module.exports.help = {
-        name: "record"
-    };
+module.exports.help = {
+    name:'record'
+}
